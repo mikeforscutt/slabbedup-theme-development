e549d63613031cb492c56db1a38fb557206b3b59
mikeforscutt <mikeforscutt@gmail.com>
2022-05-29 22:26:36 +0100

    updated method for creating shop to use hooks

--- functions.php
+++ functions.php
@@ -46,4 +46,37 @@
   
  }
 
- add_action('after_setup_theme', 'slabbedup_config', 0);
+ add_action( 'after_setup_theme', 'slabbedup_config', 0 );
+
+add_action( 'woocommerce_before_main_content', 'slabbedup_open_container_row', 5 );
+function slabbedup_open_container_row(){
+	echo '<div class="container shop-content"><div class="row">';
+}
+
+add_action( 'woocommerce_after_main_content', 'slabbedup_close_container_row', 5 );
+function slabbedup_close_container_row(){
+	echo '</div></div>';
+}
+
+add_action( 'woocommerce_before_main_content', 'slabbedup_add_sidebar_tags', 6 );
+function slabbedup_add_sidebar_tags(){
+	echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
+}
+
+remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
+add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
+
+add_action( 'woocommerce_before_main_content', 'slabbedup_close_sidebar_tags', 8 );
+function slabbedup_close_sidebar_tags(){
+	echo '</div>';
+}
+
+add_action( 'woocommerce_before_main_content', 'slabbedup_add_shop_tags', 9 );
+function slabbedup_add_shop_tags(){
+	echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
+}
+
+add_action( 'woocommerce_after_main_content', 'slabbedup_close_shop_tags', 4 );
+function slabbedup_close_shop_tags(){
+	echo '</div>';
+}
